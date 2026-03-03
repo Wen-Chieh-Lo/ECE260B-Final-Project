cab351f973ed7d1a10d7a0be33823b9461f6ee15
inQuitiVe <ericcm509mh@gmail.com>
2026-03-03 10:34:26 -0800

    Setup Step2, 3 environment. Finish sram netlist and core synth w/o sram. New core and sram netlist pass gls

--- workspace/Step1/pnr/scripts/innovus.tcl
+++ workspace/Step1/pnr/scripts/innovus.tcl
@@ -1,8 +1,8 @@
-source scripts/loadDesignTech.tcl
-source scripts/initialFloorplan.tcl
-source scripts/pinPlacement.tcl
-source scripts/placement.tcl
-source scripts/clock.tcl
-source scripts/route.tcl
-source scripts/reportDesign.tcl
-source scripts/outputGen.tcl
+source ./loadDesignTech.tcl
+source ./initialFloorplan.tcl
+source ./pinPlacement.tcl
+source ./placement.tcl
+source ./clock.tcl
+source ./route.tcl
+source ./reportDesign.tcl
+source ./outputGen.tcl
