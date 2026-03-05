7c2b9135325a6d5ae02a5445488a857fac741041
Bhonsle <sabhonsle@ieng6-ece-01.ucsd.edu>
2026-03-05 10:01:17 -0800

    Modified pinplacement script

--- workspace/Step3/pnr/sram_pnr/scripts/pinPlacement.tcl
+++ workspace/Step3/pnr/sram_pnr/scripts/pinPlacement.tcl
@@ -1,22 +1,24 @@
 ############################################
-# Pin Placement Script - Single Core
-# Inputs  : West (Left)
-# Outputs : South (Bottom)
+# SRAM Pin Placement (Step 3 - Hierarchical)
 ############################################
 
-# ---------- INPUT PINS (WEST) ----------
+# Allow batch pin editing
 setPinAssignMode -pinEditInBatch true
 
-editPin -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Left -layer 3 -spreadType center -spacing 0.8 -pin {clk reset inst[*] mem_in[*]}
+############################################
+# D input bus -> Bottom edge (M4)
+############################################
+editPin -fixOverlap 1 -unit MICRON -side Bottom -layer 4 -spreadType center -spreadDirection clockwise -spacing 4 -pin {D[*]}
 
+############################################
+# Q output bus -> Top edge (M4)
+############################################
+editPin -fixOverlap 1 -unit MICRON -side Top -layer 4 -spreadType center -spreadDirection clockwise -spacing 4 -pin {Q[*]}
+
+############################################
+# Control / address pins -> Left edge
+############################################
+editPin -fixOverlap 1 -unit MICRON -side Left -layer 3 -spreadType center -spreadDirection clockwise -spacing 4 -pin {CLK WE RE CE ADDR[*]}
+
+# End batch mode
 setPinAssignMode -pinEditInBatch false
-
-
-# ---------- OUTPUT PINS (SOUTH) ----------
-setPinAssignMode -pinEditInBatch true
-
-editPin -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Bottom -layer 4 -spreadType center -spacing 0.6 -pin {out[*]}
-
-setPinAssignMode -pinEditInBatch false
-
-checkPinAssignment
