4c138d50bbcbf59cf39657f476d59fc786801ace
AtmostAtmosphere <jaredwu910315@gmail.com>
2026-03-06 02:11:44 -0800

    Complete Step3 hier PnR(3 DRC errors)

--- workspace/Step3/pnr/hier_pnr/scripts/initialFloorplan.tcl
+++ workspace/Step3/pnr/hier_pnr/scripts/initialFloorplan.tcl
@@ -1,17 +1,17 @@
 # Floorplan
-floorPlan -site core -s 1000 1000 50.0 50.0 50.0 50.0
+floorPlan -site core -r 1 0.4 20.0 20.0 20.0 20.0
 
 timeDesign -preplace -prefix preplace
 
 globalNetConnect VDD -type pgpin -pin VDD -inst * -verbose
 globalNetConnect VSS -type pgpin -pin VSS -inst * -verbose
 
-addRing -spacing {top 1 bottom 1 left 1 right 1} -width {top 2 bottom 2 left 2 right 2}  -layer {top M1 bottom M1 left M2 right M2} -center 1 -type core_rings -nets {VSS  VDD}
+addRing -spacing {top 2 bottom 2 left 2 right 2} -width {top 4 bottom 4 left 4 right 4}  -layer {top M1 bottom M1 left M2 right M2} -center 1 -type core_rings -nets {VSS  VDD}
 
 setAddStripeMode -break_at {block_ring}
 
 ### Note: Change the number of strip  by looking at the layout #########
-#addStripe -nets {VDD VSS} -layer M4 -direction horizontal -width 2 -spacing 6 -number_of_sets 40 -start_from left -start 20 -stop 1180
+#addStripe -nets {VDD VSS} -layer M4 -direction horizontal -width 2 -spacing 5 -number_of_sets 40 -start_from left -start 20 -stop 1180
 
 addStripe -skip_via_on_wire_shape Noshape -block_ring_top_layer_limit M1 -max_same_layer_jog_length 0.8 -padcore_ring_bottom_layer_limit M1 -number_of_sets 25 -ybottom_offset 5 -skip_via_on_pin Standardcell -stacked_via_top_layer M8 -padcore_ring_top_layer_limit M1 -spacing 4 -merge_stripes_value 0.1 -direction horizontal -layer M5 -block_ring_bottom_layer_limit M1 -ytop_offset 5 -width 1 -area {} -nets {VDD VSS} -stacked_via_bottom_layer M1
 
