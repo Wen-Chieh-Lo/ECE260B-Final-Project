46c9ed302e94457934769cd59ffdfc9bd57ecccf
AtmostAtmosphere <jaredwu910315@gmail.com>
2026-03-05 21:39:40 -0800

    Modified floor planning (reducing DRC errors)

--- workspace/Step3/pnr/hier_pnr/scripts/pinPlacement.tcl
+++ workspace/Step3/pnr/hier_pnr/scripts/pinPlacement.tcl
@@ -7,7 +7,7 @@
 # ---------- INPUT PINS (WEST) ----------
 setPinAssignMode -pinEditInBatch true
 
-editPin -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Left -layer 3 -spreadType center -spacing 0.8 -pin {clk reset inst[*] mem_in[*]}
+editPin -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Left -layer 3 -spreadType center -spacing 4 -pin {clk reset inst[*] mem_in[*]}
 
 setPinAssignMode -pinEditInBatch false
 
@@ -15,7 +15,7 @@ setPinAssignMode -pinEditInBatch false
 # ---------- OUTPUT PINS (SOUTH) ----------
 setPinAssignMode -pinEditInBatch true
 
-editPin -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Bottom -layer 4 -spreadType center -spacing 0.6 -pin {out[*]}
+editPin -fixOverlap 1 -unit MICRON -spreadDirection clockwise -side Bottom -layer 4 -spreadType center -spacing 4 -pin {out[*]}
 
 setPinAssignMode -pinEditInBatch false
 
