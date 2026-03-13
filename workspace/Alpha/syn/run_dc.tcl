e4beb8c0bc4146ac3b7b14e51c1c2655cd405102
Lo <w4lo@ieng6-ece-04.ucsd.edu>
2026-03-13 14:56:06 -0700

    Fixed Alpha WNS violation

--- workspace/Alpha/syn/run_dc.tcl
+++ workspace/Alpha/syn/run_dc.tcl
@@ -27,7 +27,7 @@ if { [file exists $filelist_full] } {
 }
 
 # Target library
-set target_library /home/linux/ieng6/ECE260B_WI26_A00/public/PDKdata/db/tcbn65gplustc.db
+set target_library /home/linux/ieng6/ECE260B_WI26_A00/public/PDKdata/db/tcbn65gpluswc.db
 set link_library $target_library
 set symbol_library {}
 set wire_load_mode enclosed
