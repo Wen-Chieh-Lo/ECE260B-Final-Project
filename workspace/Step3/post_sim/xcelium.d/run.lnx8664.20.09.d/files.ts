66d15f0ea94111bf5a8e66292d57ba5ace4ad8cf
inQuitiVe <ericcm509mh@gmail.com>
2026-03-14 02:54:04 -0700

    Step3 post-sim pass

---
+++ workspace/Step3/post_sim/xcelium.d/run.lnx8664.20.09.d/files.ts
@@ -0,0 +1,4 @@
+1772520018 /home/linux/ieng6/ECE260B_WI26_A00/c8chiu/ECE260B-Final-Project/workspace/Step3/post_sim/netlist/tcbn65gplus.v
+1773479037 /home/linux/ieng6/ECE260B_WI26_A00/c8chiu/ECE260B-Final-Project/workspace/Step3/post_sim/netlist/core.pnr.v
+1773480208 /home/linux/ieng6/ECE260B_WI26_A00/c8chiu/ECE260B-Final-Project/workspace/Step3/post_sim/netlist/sram_w16.pnr.v
+1773479217 /home/linux/ieng6/ECE260B_WI26_A00/c8chiu/ECE260B-Final-Project/workspace/Step3/post_sim/netlist/step1_tb.v
