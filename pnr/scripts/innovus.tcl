c272d8b42661e24e0ba94b519bb531861a7609f6
inQuitiVe <ericcm509mh@gmail.com>
2026-03-21 19:43:56 -0700

    cleanup

--- pnr/scripts/innovus.tcl
+++
@@ -1,8 +0,0 @@
-source scripts/loadDesignTech.tcl
-source scripts/initialFloorplan.tcl
-source scripts/pinPlacement.tcl
-source scripts/placement.tcl
-source scripts/clock.tcl
-source scripts/route.tcl
-source scripts/reportDesign.tcl
-source scripts/outputGen.tcl
